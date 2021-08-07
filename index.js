0b2ef3292fd02279be000789f2008c03b73c512f
raikar651 <rasikraikar@gmail.com>
2021-08-07 10:36:13 +0530

    save changes to github

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
-
+var nav_block=document.querySelector(".nav-items");
+var nav_icon1=document.querySelector(".fa-times");
+var nav_icon2=document.querySelector(".fa-bars");
 function showMenu()
 {
-    var nav_block=document.getElementsByClassName("check-class"));
-    nav_block.style.left="0";
+    nav_block.style.top="100%";
+    nav_icon1.style.visibility="visible";
+    nav_icon2.style.visibility="hidden";
+    
+}
+function hideMenu()
+{
+    nav_block.style.top="-400%";
+    nav_icon1.style.visibility="hidden";
+    nav_icon2.style.visibility="visible";
     
 }
